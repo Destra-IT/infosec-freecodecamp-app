a66acb2f4cc898838d2a3ef180b590929d0be34f
Destra-IT <adrien.duhem.corp@gmail.com>
2024-06-12 16:35:07 +0200

    Ajout de la protection contre les frame / iframe debug 3

--- myApp.js
+++ myApp.js
@@ -3,11 +3,7 @@ const helmet = require('helmet');
 const app = express()
 
 app.use(helmet.hidePoweredBy());
-app.use(
-  helmet.frameguard({
-    xFrameOptions: { action: "deny" },
-  })
-);
+app.use(helmet.frameguard({ action: "deny"})); 
 
 
 
